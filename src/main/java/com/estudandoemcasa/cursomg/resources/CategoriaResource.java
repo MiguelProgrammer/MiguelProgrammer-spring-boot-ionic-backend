492331d735c9f3467bafa1fe79a81588794112f8
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2021-08-05 23:21:36 -0300

    Testando primeira classe de dominio

--- src/main/java/com/estudandoemcasa/cursomg/resources/CategoriaResource.java
+++ src/main/java/com/estudandoemcasa/cursomg/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.estudandoemcasa.cursomg.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.estudandoemcasa.cursomg.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")// <- endpoint rest
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando!";
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1,"Informatica"); 
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
 	}
 }

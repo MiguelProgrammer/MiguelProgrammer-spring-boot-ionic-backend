1211d1faeac7529197a877824b09dcffd8a79f6c
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2021-08-05 22:14:59 -0300

    Projeto Criado

---
+++ src/main/java/com/estudandoemcasa/cursomg/CursomgApplication.java
@@ -0,0 +1,13 @@
+package com.estudandoemcasa.cursomg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudandoemcasa/cursomg/CursomgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudandoemcasa.cursomg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

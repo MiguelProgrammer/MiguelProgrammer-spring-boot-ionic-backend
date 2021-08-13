dc81158c44afdbdef55ef308d863e710d1307ffd
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2021-08-13 00:32:53 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/estudandoemcasa/cursomg/resources/ClienteResource.java
@@ -0,0 +1,26 @@
+package com.estudandoemcasa.cursomg.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudandoemcasa.cursomg.domain.Cliente;
+import com.estudandoemcasa.cursomg.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")// <- endpoint rest
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService servico;
+
+	@RequestMapping(value="/{id}" ,method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+
+		Cliente obj = servico.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}	
+}
---
+++ src/main/java/com/estudandoemcasa/cursomg/services/ClienteService.java
@@ -0,0 +1,27 @@
+package com.estudandoemcasa.cursomg.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudandoemcasa.cursomg.domain.Categoria;
+import com.estudandoemcasa.cursomg.domain.Cliente;
+import com.estudandoemcasa.cursomg.repositories.ClienteRepository;
+import com.estudandoemcasa.cursomg.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id){
+		Optional<Cliente> obj = repo.findById(id);
+		if(obj.isEmpty()) {
+			throw new ObjectNotFoundException("Objeto não encontrado. ID = " + id +" "
+					+ ", Tipo: " + Categoria.class.getName());
+		}
+		return obj.orElse(null);
+	}
+}

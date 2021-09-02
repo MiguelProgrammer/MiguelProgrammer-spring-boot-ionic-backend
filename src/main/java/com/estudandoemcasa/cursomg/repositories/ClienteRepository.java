a9f326a3dbfc47550a975671ecf50cf46731ff5b
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2021-09-02 00:41:30 -0300

    Validação customizada: email não repetido no cadastro de cliente

--- src/main/java/com/estudandoemcasa/cursomg/repositories/ClienteRepository.java
+++ src/main/java/com/estudandoemcasa/cursomg/repositories/ClienteRepository.java
@@ -2,6 +2,7 @@ package com.estudandoemcasa.cursomg.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.estudandoemcasa.cursomg.domain.Cliente;
 /*
@@ -10,4 +11,6 @@ import com.estudandoemcasa.cursomg.domain.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente,Integer>{
 
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/estudandoemcasa/cursomg/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/estudandoemcasa/cursomg/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.estudandoemcasa.cursomg.domain.Cliente;
 import com.estudandoemcasa.cursomg.domain.enums.TipoCliente;
 import com.estudandoemcasa.cursomg.dto.ClienteNewDTO;
+import com.estudandoemcasa.cursomg.repositories.ClienteRepository;
 import com.estudandoemcasa.cursomg.resources.exception.FieldMessage;
 import com.estudandoemcasa.cursomg.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO>{
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	public void initialize(ClienteInsert ann) {
 	}
@@ -24,6 +31,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 				objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()))
 				&& !BR.isValidCodigoPessoa(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "Identificador de Tipo de Pessoa inválido!"));
+		}
+		
+		Cliente laranja = clienteRepository.findByEmail(objDto.getEmail());
+		if(laranja != null) {
+			list.add(new FieldMessage("Email", "Email já está cadastrado!"));
 		}
 		
 		for(FieldMessage e : list) {
